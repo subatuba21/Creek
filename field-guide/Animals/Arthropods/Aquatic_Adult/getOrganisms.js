69be9fd09c8b9461d83700f0da96587e01258135
subatuba21 <rohan6056@hotmai.com>
2020-01-20 08:05:45 -0800

    Field Guide updates

---
+++ field-guide/Animals/Arthropods/Aquatic_Adult/getOrganisms.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var readline = require("readline");
+var r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+r1.question("Path: ", function(path) {
+
+});
---
+++ makeGFol.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const readline = require('readline');
+const getPath = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var str = "";
+var first="";
+
+getPath.question("Path: ", (path)=>{
+  var dir = fs.opendirSync(path);
+  var remaining = true;
+  getFiles(dir, path);
+});
+
+function getFiles(dir) {
+  dir.read().then(file=> {
+    if (file==null) {
+      throw new Error();
+    }
+    console.log(file.name);
+    if (file.name.search(/(.jpg|.png)/i)!=-1) {
+      str+=`${first}${file.name}`;
+      if (first=="") {
+        first=",";
+      }
+    }
+    getFiles(dir);
+  }).catch(function() {
+    console.log("All done!");
+    fs.writeFile("recentIml.txt", str, (err)=>{
+      console.log("File created");
+      dir.close();
+    });
+  });
+}
